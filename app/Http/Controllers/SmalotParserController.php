a8e0cdd2a646921eb5288613f80d815d7992138b
joaopsj <jacksoublack@gmail.com>
2022-04-11 16:17:07 -0300

    Criado duas novas controllers

---
+++ app/Http/Controllers/SmalotParserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+/**
+ * Classe para verificar se o arquivo PDF é texto ou imagem
+ * @TODO Criar função para validar o arquivo e enviar para Controller correta, 
+ * ImagemParaTextoController ou PdfParaTextoController
+ * 
+ */
+
+class SmalotParserController extends Controller
+{
+    public function smtparser()
+    {
+        // Parse PDF file and build necessary objects.
+        $parser = new \Smalot\PdfParser\Parser();
+        $pdf = $parser->parseFile('c:\projetos-git\inep.pdf');
+        
+        $text = $pdf->getText();
+       
+        if($text === 1)
+        {
+           /**
+            * O teste de pdf para texto falhou, o PDF pode ser uma imagem.
+            * @TODO ImagemParaTextoController
+            */
+        } else {
+            /**
+             * O teste foi validado, o arquivo é um PDF é texto
+             * @TODO PdfParaTextoController 
+             */
+        }
+
+
+        echo $text;
+        return true;
+    }
+    
+}
