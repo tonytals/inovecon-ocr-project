1062b8a3552c9a9891b17d87d659d69fbe195c17
tonytals <webtals@gmail.com>
2022-05-05 19:06:32 -0300

    Interação PDF para IMAGEM. TODO: Mover código para camada correta

--- app/Http/Controllers/PdfParaImagemController.php
+++ app/Http/Controllers/PdfParaImagemController.php
@@ -12,12 +12,28 @@ class PdfParaImagemController extends Controller
 {
     public function PdFParaImagem()
     {
-       // dd(public_path() . "\storage\pdf\SENTENCA_2021.pdf");            
+       //dd(public_path() . "/storage/pdf/SENTENCA_2021.pdf");
         
-        $pathToPdf = public_path() . "\storage\pdf\SENTENCA_2021.pdf" ;
-        $pathToWhereImageShouldBeStored = public_path() . "\storage\pdf\img";
+        $pathToPdf = public_path() . "/storage/pdf/SENTENCA_2021.pdf" ;
+        $pathToWhereImageShouldBeStored = public_path() . "/storage/pdf/img";
         $pdf = new Pdf($pathToPdf);
-        $pdf->saveImage($pathToWhereImageShouldBeStored);
+
+        /**
+         * Iteração para renderizar todas as páginas do pdf
+         * @TODO MOVER O TRECHO DE CÓDIGO PARA A CAMADA DE SERVIÇO (services)
+         */
+        $quantidadeDePaginas = $pdf->getNumberOfPages();
+
+        for( $i = 1; $i <= $quantidadeDePaginas; $i++)
+        {
+            $pathToWhereImageShouldBeStored = public_path() . "/storage/pdf/img_" . $i . ".jpg";
+            $pdf->setPage($i)->saveImage($pathToWhereImageShouldBeStored);
+            echo "Imagem Salva: " . $pathToWhereImageShouldBeStored . "\n";
+
+        }
+
+        return "Imagens geradas com sucesso!";
+
 
     }
         
